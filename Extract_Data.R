a109e30c60bc38497cd73ef04026bed7ceb2da51
bullocke <bullocke@bu.edu>
2016-03-10 18:29:48 -0500

    Update extraction script

--- Extract_Data.R
+++ Extract_Data.R
@@ -1,20 +1,61 @@
 ##Function for retrieving data and outputting as a CSV. 
 
-library(raster) 
-library(gdalUtils)
-library(rgdal)
-library(MODIS) 
+library(raster, quietly=TRUE,warn.conflicts=FALSE) 
+library(gdalUtils, quietly=TRUE,warn.conflicts=FALSE)
+library(rgdal, quietly=TRUE,warn.conflicts=FALSE)
+library(optparse, quietly=TRUE,warn.conflicts=FALSE)
+library(rasterVis, quietly=TRUE,warn.conflicts=FALSE)
+library(sp, quietly=TRUE,warn.conflicts=FALSE)
 
-setwd('/Users/ericbullock/Google Drive/Class/Ecological_Forecasting/Project')
+suppressPackageStartupMessages(library("optparse"))
+option_list = list(
+  make_option(c("-m", "--modis"), action="store", default=NA, type='character', 
+              help="Path to MODIS file"),
+  make_option(c("-g", "--GPM"), action="store", default=NA, type='character',
+              help="Path to GPM file"),
+  make_option(c("-s", "--SMAP"), action="store", default=NA, type='character',
+              help="Path to SMAP file")
+)
+opt = parse_args(OptionParser(option_list=option_list))
+inputs=list()
+num=1
 
-get_ndvi <- function(point, image){
+if (!is.na(opt$modis)) {
+  inputs[num] <- opt$modis
+  num <- num + 1
+}
+
+if (!is.na(opt$GPM)) {
+  inputs[num] <- opt$GPM
+  num <- num + 1
+}
+
+if (!is.na(opt$SMAP)) {
+  inputs[num] <- opt$SMAP
+  num <- num + 1
+}
+
+if (length(inputs)==0) {
+  stop("No files to process", call.=FALSE)
+} 
+
+get_ndvi <- function(roi, image){
+  ###Function to get today's NDVI at point of interest. 
+  
+  #Inputs:
+    #roi: Point of interest. Format: SpatialPoints
+    #image: MODIS surface reflectance image. Format: .hdf file 
+  
+  #Outputs:
+    #NDVI: NDVI value at roi. Format: integer
+    #Today: Today's date. Format: Date. 
+  
   #Get today's date for the temp files. 
   today <- Sys.Date()
-  
   #Get subdatasets from the HDF file. 
-  hesubdatasets <- get_subdatasets(image)
-  red <- grep('b03',subdatasets)
-  nir <- grep('b04', subdatasets)
+  subdatasets <- get_subdatasets(image)
+  red <- grep('b01',subdatasets)
+  nir <- grep('b02', subdatasets)
   redband <- subdatasets[red]
   nirband <- subdatasets[nir]
 
@@ -27,38 +68,155 @@ get_ndvi <- function(point, image){
     outred <- paste(today,'_red.tif',sep='')
     outnir <- paste(today,'_nir.tif',sep='')
     gdal_translate(redband, outred, of = 'GTiff')
-    gdal_translate(redband, outnir, of = 'GTiff')
+    gdal_translate(nirband, outnir, of = 'GTiff')
     redraster <- raster(outred)
     nirraster <- raster(outnir)
   }  
-    
+  msg <- {}
+  
+  #Calculate NDVI
   ndvi <- (nirraster - redraster) / (redraster + nirraster)
   ndvi <- raster(ndvi)
-  roi <- readOGR('Point.shp','Point')
   
-  ndvi_value <- extract(redraster,roi)
-  
+  #Read in ROI data and project to the MODIS Sinusoidal Projection
+  projection(roi) <- CRS("+proj=lonlat +ellps=WGS84")
+  roi_rep <- spTransform(roi, CRS(projection(ndvi)))
+  ndvi_value <- extract(ndvi,roi_rep)
   return(ndvi_value)
 }
 
-get_soil_moisture <- function(point,image) {
-
+get_soil_moisture <- function(roi,image) {
+  
+  ###Function to get today's soil moisture value at point of interest. 
+  
+  #Inputs:
+    #roi: Point of interest. Format: SpatialPoints
+    #image: SMAP surface moisture image. Format: .hdf file 
+  
+  #Outputs:
+    #soil_m: Soil moisture value at roi. Format: integer
+    #Today: Today's date. Format: Date. 
+  
   today <- Sys.Date()
   
   #Get subdatasets from the HDF file. 
-  subdatasets <- get_subdatasets('smau.h2')
-  soilmoisture <- grep('sm_profile_analysis',subdatasets)
+  subdatasets <- get_subdatasets('SMAP_L3_SM_P_20151126_R12170_001.h5')
+  soilmoisture <- grep('soil_moisture',subdatasets)
   smband <- subdatasets[soilmoisture[1]]
+
   
   #Try using hdf4 bindings to directly read in data. 
   soilmoistureraster <- try(raster(smband))
+
+
   msg <- geterrmessage()
   if (grepl("Cannot create a RasterLayer", msg)) {
     print('Using alternative method to read in data')
     outsm <- paste(today,'_sm.tif',sep='')
-    outnir <- paste(today,'_nir.tif',sep='')
-    gdal_translate(redband, outsm, of = 'GTiff')
+    latsm <- paste(today,'_lat.tif',sep='')
+    longsm <- paste(today,'_long.tif',sep='')
+    gdal_translate(smband, outsm, of = 'GTiff')
     soilmoistureraster <- raster(outsm)
   }  
+  msg <- {}
+  
+  #Set projection of ROI
+  projection(roi) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+
+  #Set EASE2 Grid projection for SMAP data
+  proj4string(soilmoistureraster) <- CRS("+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ")
+
+  
+  #Realign and reproject EASE grid raster onto WGS84 Lat/Long grid
+  ex=extent(-17327926.6562500000000000,17340460.418750010430812,-7338517.9437500033527613,7338517.9437499996274710)
+  sm_r <- setExtent(soilmoistureraster,ex)
+
+
+
+  #Reproject ROI to projection of SMAP data
+  roi_rep <- spTransform(roi, CRS(projection(soilmoistureraster)))
+  sm_r <- setExtent(soilmoistureraster,ex)
+
+  soil_m <- extract(sm_r,roi_rep)
+
+  return(soil_m)
 }
 
+get_GPM<- function(roi,image) {
+  
+  ###Function to get today's rainfall value at point of interest. 
+  
+  #Inputs:
+  #roi: Point of interest. Format: SpatialPoints
+  #image: SMAP surface moisture image. Format: .hdf file 
+  
+  #Outputs:
+  #rainfall: Rainfall value at roi. Format: integer
+  #Today: Today's date. Format: Date. 
+  
+  today <- Sys.Date()
+
+  #Open the image
+  rainfallraster <- try(raster(image))
+  msg <- geterrmessage()
+  if (grepl("Cannot create a RasterLayer", msg)) {
+    print('File is missing')
+  }  
+  msg <- {}
+  
+  #Assign the projection and extents
+  projection(roi) <- CRS("+proj=lonlat +ellps=WGS84")
+  projection(rainfallraster) <- CRS("+proj=lonlat +ellps=WGS84")
+  ex=extent(-180, 180, -90, 90)
+  rain_ext <- setExtent(rainfallraster,ex)
+
+  
+  #Retrieve coordinates of point and extract data at point. 
+  rain  <- extract(rain_ext,roi)
+
+  return(soil_m)
+}
+
+write_csv <- function(today, value, data) {
+  csv=paste(data,'.csv',sep='')
+  r_csv <- read.csv(csv,sep=',',stringsAsFactors=FALSE,header=TRUE)
+  Date<-as.character(today)
+  value=3
+  SoilMoisture<-as.character(value)
+  out_df <- data.frame(Date,SoilMoisture)
+  out_csv <- rbind(r_csv,test)
+  write.csv(out_csv,csv)
+}
+
+#Start actual code
+
+
+
+#Define region of interest
+long=-98.01
+lat=29.93
+coords <- as.data.frame(cbind(long, lat))
+roi <- SpatialPoints(coords)
+
+#Today's date
+today <- Sys.Date()
+
+#Retrieve data for 3 datasets
+inputs=list('SMAP')
+for (i in inputs){
+  in_name <- substr(i,0,3)
+  if (in_name == "MOD") {
+    value <- get_ndvi(roi, image)
+    write_csv(today, value, 'MODIS')
+  }
+  else if (in_name =="GPM") {
+    data <- get_GPM(roi, image)
+    write_csv(today, value, 'GPM')
+  }
+  else if (in_name == "SMA") {
+    data <- get_soil_moisture(roi, image)
+    write_csv(today, value, 'SMA')
+  }
+
+}
