4d5c5e391bf92705fc08bada3ff23487d164d157
Eric Bullock <bullocke@bu.edu>
2016-03-25 17:00:04 -0400

    bug fix

--- 02_Extract_Data.R
+++ 02_Extract_Data.R
@@ -85,7 +85,6 @@ get_ndvi <- function(roi, image, day){
   roi_rep <- spTransform(roi, CRS(projection(ndvi)))
   ndvi_value <- extract(ndvi,roi_rep)
   cloud_int <- extract(cloudraster,roi_rep)
-<<<<<<< HEAD
 #  get.pkg('R.utils')
   #cloud_bit <- intToBin(cloud_int)
   cloud_bit <- as.numeric(intToBits(cloud_int))
@@ -102,13 +101,6 @@ get_ndvi <- function(roi, image, day){
 
   if (!(QA_ST0_1==0 & QA_ST2==0 & QA_ST10==0 & QA_ST11==0 & QA_ST12==0 & QA_ST13==0 & QA_ST15==0 & QA_ST8_9<=1 & (QA_ST6_7==1 |QA_ST6_7==2))) {
      ndvi_value = NA
-=======
-  get.pkg('R.utils')
-  cloud_bit <- intToBin(cloud_int)
-  cloud_value <- substr(cloud_bit,11,11)
-  if (cloud_value == "1") {
-     ndvi_value <- -9999
->>>>>>> 63dbedfb3461cc5df4c993d6ba0602c7e640002c
   }
 
   return(ndvi_value)
@@ -237,14 +229,7 @@ if (in_name == "MOD") {
     print(date_unformatted)
     Date <- strptime(date_unformatted, "%Y%j")
     value <- get_ndvi(roi, input, date_unformatted)
-<<<<<<< HEAD
     write_csv(Date, value, 'MODIS')
-=======
-    if (as.integer(value) > -9998) {
-         write_csv(Date, value, 'MODIS')
-    } else {
-      print("Too cloudy to calculate")
->>>>>>> 63dbedfb3461cc5df4c993d6ba0602c7e640002c
     }
   } else if (in_name =="GPM") {
     date_unformatted <- as.integer(substr(input,33,40))
@@ -259,9 +244,8 @@ if (in_name == "MOD") {
          write_csv(Date, value, 'SMAP')
      } else {
          value <- NA
-	 write_csv(Date, value, 'SMAP')
+     write_csv(Date, value, 'SMAP')
      print('No data for today for SMAP') }
 } else {
   print('ERROR: Unrecognized file format')
 }
-
