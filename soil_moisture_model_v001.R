430d3f0d2482f3a1a305dce70d585f450253dded
condontd <condontd@bu.edu>
2016-04-04 18:55:44 -0400

    Adding NDVI in to the model. changed n's to t's.

--- soil_moisture_model_v001.R
+++ soil_moisture_model_v001.R
@@ -4,40 +4,40 @@
 
 ###################
 #------------------ sub-routines,set your JAGS model here
-predict.JAGS <- function(time,y,p) {
+predict.JAGS <- function(time,y,p,t,v) {
   require(rjags)
   require(coda)
-
+  
   SoilMoisturePrecipFusion = "
   model{
   
   #### Data Model
-  for(i in 1:n){
+  for(i in 1:t){
   y[i] ~ dnorm(x[i],tau_obs)
   }
   
   #### Process Model
-  for(i in 2:n){
-  SoilMoisture[i] <- beta_0*x[i-1] + beta_1*p[i]
+  for(i in 2:t){
+  SoilMoisture[i] <- beta_0*x[i-1] + beta_1*p[i] - beta_2*n[1]*x[i-1]
   x[i]~dnorm(SoilMoisture[i],tau_add)
   }
-  
-  
   
   #### Priors
   tau_obs ~ dgamma(a_obs,r_obs)
   tau_add ~ dgamma(a_add,r_add)
   beta_0 ~ dbeta(a_beta0,r_beta0)
   beta_1 ~ dgamma(a_beta1,r_beta1)
+  beta_2 ~ dgamma(a_beta2,r_beta2)
   ## initial condition
   x[1] ~ dunif(x_ic_lower,x_ic_upper)  
-}
-"
+  }
+  "
   
-  data <- list(y=log(y),p=p, n=length(y),x_ic_lower=log(0.000001),x_ic_upper=log(1), a_obs=0.01,
-               r_obs=0.01,a_add=0.01, r_add=1, a_beta0=1,r_beta0=0.5, a_beta1=1, r_beta1=0.001)
-
-
+  data <- list(y=log(y),p=p,n=n, t=length(y),x_ic_lower=log(0.000001),x_ic_upper=log(1), a_obs=0.01,
+               r_obs=0.01,a_add=0.01, r_add=1, a_beta0=1,r_beta0=0.5, a_beta1=1, r_beta1=0.001,
+               a_beta2=0.05,r_beta2=9)
+  
+  
   
   nchain = 3
   init <- list()
@@ -58,7 +58,7 @@ predict.JAGS <- function(time,y,p) {
   # Only to plot 1000 iterations.  
   
   plot(jags.out) 
-
+  
   
   jags.out   <- coda.samples (model = j.model,
                               variable.names = c("x","tau_add","tau_obs"),
@@ -98,7 +98,7 @@ n = combined$NDVI
 # plot original weekly observation data
 plot(time,y,type='l',ylab="SoilMoisture",lwd=2,main='Daily SoilMoisture')
 
-jags.out.original = predict.JAGS(time,y, p)
+jags.out.original = predict.JAGS(time,y, p,t,n)
 
 
 par(mfrow=c(1,1))
